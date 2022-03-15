9b7d3291638d9b1accd63dd75f9675687d268c30
Samuel <lutumbasamuel9@gmail.com>
2022-03-15 12:48:35 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,12 @@
 
 /**
  * _islower - check for lower case letter
+ * @c: character to check the case
  * Return: 0 or 1
  */
 int _islower(int c)
 {
-if (c >= 97 && c <=122)
+if (c >= 97 && c <= 122)
 return (1);
 else
 return (0);
