27a07bee76eb1ccc4f18ea0966ed042b2a2d1868
Samuel <lutumbasamuel9@gmail.com>
2022-03-15 09:49:39 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: program prints _putchar
+ * Return: 0 when successul
+ */
+int main(void)
+{
+char text[9] = "_putchar";
+int i;
+for (i = 0; i < 9; i++)
+{
+_putchar(text[i]);
+}
+_putchar('\n');
+return (0);
+}
