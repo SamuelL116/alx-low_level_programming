4060ff4f4f696a0c37fac337d84979d561ce5337
Samuel <lutumbasamuel9@gmail.com>
2022-03-15 16:17:30 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,46 +2,27 @@
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- * Return:void
+ * starting from 00:00 to 23:59.
+ * Return 0
  */
 
 void jack_bauer(void)
 {
-
-int h1;
-int h2;
-int m1;
-int m2;
-int a = 9;
-
-h2 = 0;
-while (h2 <= 2)
+int x = 0;
+int j = 0;
+while (x <= 23)
 {
-if (h2 == 2)
+j = 0;
+while (j <= 59)
 {
-/*Restrain to 23h, not 29*/
-a = 3;
-}
-h1 = 0;
-while (h1 <= a)
-{
-m2 = 0;
-while (m2 <= 5)
-{
-m1 = 0;
-while (m1 <= 9)			{
-_putchar('0' + h2);
-_putchar('0' + h1);
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
 _putchar(':');
-_putchar('0' + m2);
-_putchar('0' + m1);
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
 _putchar('\n');
-m1++;
+j += 1;
 }
-m2++;
-}
-h1++;
-}
-h2++;
+x += 1;
 }
 }
