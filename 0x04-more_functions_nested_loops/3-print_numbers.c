acec6920e2f69b49487ddecd53d023890035dc7f
Samuel <lutumbasamuel9@gmail.com>
2022-03-17 10:42:21 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers - prints 0 - 9.
+ *
+ *Return: void.
+ */
+void print_numbers(void)
+{
+int n = 0;
+while (n < 10)
+{
+_putchar(n + '0');
+n +=1;
+}
+_putchar('\n');
+}
