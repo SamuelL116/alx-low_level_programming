72d3e0f6aef024e3e1e9997e7e5b887c8b88c9a8
Samuel <lutumbasamuel9@gmail.com>
2022-03-17 09:18:08 -0700

    0-isupper.c added

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,9 @@
 /**
  *_isdigit - checks for a digit.
  *@c: input.
- *
- *Return: 1 if c is a digit
- *0 otherwise.
+ *Return: 1 if c is a digit, 0 otherwise.
  */
-int _isdigit(int c)
+int _isupper(int c)
 {
 if ((c >= 'A') && (c <= 'Z'))
 {
