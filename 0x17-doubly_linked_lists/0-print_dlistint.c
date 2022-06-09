73c710d2dba756c28c66a6a4d73fd0205b44d781
Samuel <lutumbasamuel9@gmail.com>
2022-06-09 14:44:45 -0700

    lists.h

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,4 @@
  * Return: number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *n)
-{
-size_t node = 0;
-
-while (h)
-{
-node++;
-printf("%d\n", h->n);
-h = h->next;
-}
-return (node);
 }
