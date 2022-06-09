a025422dcd5760007fed5cd6019d83101109faca
Samuel <lutumbasamuel9@gmail.com>
2022-06-09 14:55:16 -0700

    Betty again

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * @print_dlistint - prints all the elements of a list
+ * print_dlistint - prints all the elements of a list
  * @h: const value
  *
  * Return: number of nodes
